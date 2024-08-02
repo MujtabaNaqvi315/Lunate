b093079043591e48432a16949edcd176b9fb9869
Syed Muhammad Mujtaba Naqvi <smmujtabanaqvi315@gmail.com>
2024-08-02 03:38:32 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './styles/App.css';
 import Home from './components/Home';
 import Header from './components/Header';
 import Intro from './components/Intro';
+import Slider from './components/Slider';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Home />
       <Intro />
+      <Slider />
     </>
   );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,17 @@
 import '../styles/components/intro.scss';
+import $ from 'jquery';
 
 function Intro () {
+
+    function handleExploreLinkMouseEnter () {
+        $('.explore-link-arrow-js').removeClass('on-mouse-out');
+        $('.explore-link-arrow-js').addClass('on-mouse-in');
+    }
+
+    function handleExploreLinkMouseLeave () {
+        $('.explore-link-arrow-js').removeClass('on-mouse-in');
+        $('.explore-link-arrow-js').addClass('on-mouse-out');
+    }
 
     return (
         <div className='intro'>
@@ -11,10 +22,10 @@ function Intro () {
                 </div>
                 <div className='description-sec'>
                     <p className='description'>Lunate is your trusted investment partner. Driven by discipline, powered by expertise and defined by excellence – we enable our partners to invest at scale, building customised portfolios across strategies, asset classes and products.</p>
-                    <a className='explore-link' href="#">
+                    <a className='explore-link' onMouseEnter={handleExploreLinkMouseEnter} onMouseLeave={handleExploreLinkMouseLeave} href="#">
                         Explore Lunate
                         <span className='explore-link-span'>
-                            <svg className='explore-link-arrow' width="0.938rem" height="0.938rem" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='explore-link-arrow explore-link-arrow-js' width="0.938rem" height="0.938rem" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M8.35355 0.646447C8.15829 0.451184 7.84171 0.451184 7.64645 0.646447C7.45118 0.841709 7.45118 1.15829 7.64645 1.35355L13.7929 7.5H1.00195C0.725811 7.5 0.501953 7.72386 0.501953 8C0.501953 8.27614 0.725811 8.5 1.00195 8.5H13.7929L7.64645 14.6464C7.45118 14.8417 7.45118 15.1583 7.64645 15.3536C7.84171 15.5488 8.15829 15.5488 8.35355 15.3536L15.3282 8.37892C15.4346 8.28723 15.502 8.15148 15.502 8C15.502 7.84852 15.4346 7.71278 15.3282 7.62109L8.35355 0.646447Z"/>
                             </svg>
                         </span>
